defa7c337489a053acdcea21c8d889074eb2421e
ypablo <yfedora@gmail.com>
2019-06-07 22:42:14 +0100

    25.5 middleware

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+
+app.use('/store', function (req, res, next) {
+    console.log('Jestem pośrednikiem przy żądaniu do/ store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!')
+});
+
+app.get('/store', function (req, res) {
+    res.send("To jest sklep.");
+});
+
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
